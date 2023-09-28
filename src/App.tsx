3ac3410993401ae03328df06451bbd81497061da
vietvo <vietvonhu18@gmail.com>
2023-09-28 09:46:40 +0700

    ff

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import React, { Component } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO WORLD</h1>
+    </div>
+  );
+}
+
+export default App;
